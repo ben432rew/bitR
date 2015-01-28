4ecbf574cdef944a56a153dcf4a3907515c7649a
Benjamin Himley <benjaminhimley85@gmail.com>
2015-01-28 17:36:25 -0500

    adding models

--- bitmess/models.py
+++ bitmess/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class BitKey(models.Model):
+    key = models.CharField(max_length=200)
+    created_at = models.CharField(auto_now_add=True)
+    user = models.ForeignKey( User )
+    is_passive = models.BooleanField(default=False)
+
+
+class ReadMessage(models.Model):
+    key = models.CharField(max_length=200, primary_key=True)
