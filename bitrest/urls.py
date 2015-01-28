86977ade24c75bd23b5cb249007d041b7bf0d153
Benjamin Himley <benjaminhimley85@gmail.com>
2015-01-28 17:51:21 -0500

    beginning sign in views

--- bitrest/urls.py
+++ bitrest/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('bitrest.urls')),
-    url(r'^bitrest/', include('bitrest.urls')),
-    url(r'^bitmess/', include('bitmess.urls')),
+    url(r'^$', Index.as_view(), name="index" ),
+    url(r'^$', login_required( Index.as_view() ), name="index" ),
 )
