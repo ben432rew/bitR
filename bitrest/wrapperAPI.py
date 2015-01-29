fa3e7a08e1c459ae1eebe8be8d33346b119af484
adolfo0620 <adolfo0620@gmail.com>
2015-01-29 16:12:31 -0500

    fixed getAllMessagesIds to also use read

--- bitrest/wrapperAPI.py
+++ bitrest/wrapperAPI.py
@@ -55,6 +55,9 @@ class API():
 					dic['subject'] = base64.b64decode(value).decode()
 		return(inboxMessages['inboxMessages'])
 
-	def getAllMessagesIds(self):
-		ids = self.api.getAllInboxMessageIDs()
+	def getAllMessagesIds(self,*args):
+		if len(args)>0:
+			ids = self.api.getAllInboxMessageIDs(args[0].lower())
+		else:	
+			ids = self.api.getAllInboxMessageIDs()
 		return(json.loads(ids)["inboxMessageIds"])
