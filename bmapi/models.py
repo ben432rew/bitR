94a182225d357860cfb239f2904d1bfbe6ccf70c
Benjamin Himley <benjaminhimley85@gmail.com>
2015-01-30 17:36:35 -0500

    check_token function

--- bmapi/models.py
+++ bmapi/models.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# is_passive field is for the when the passive operating mode ability is built 
-# into bitmessage.  See chapter 7:  https://bitmessage.org/bitmessage.pdf
 class BitKey(models.Model):
     key = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey( User )
-    is_passive = models.BooleanField(default=False)
+
+
+class Token( models.Model):
+    user = models.ForeignKey( User )
+    created_at = models.DateTimeField(auto_now_add=True)
+    token = models.CharField( max_length = 50 )
