3f8e84490b74d3e991718dbb7925fa114662b41c
Benjamin Himley <benjaminhimley85@gmail.com>
2015-01-31 19:59:25 -0500

    token auth working now

--- bitweb/static/js/inbox.js
+++ bitweb/static/js/inbox.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $.material.ripples()
     $.get('/bmapi/allmessages', function (data){
+        console.log("here")
         data['messages'].forEach(function(value) {
             $.scope.inbox.push(value)
         })
