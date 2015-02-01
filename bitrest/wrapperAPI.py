9649216cccc975ef28de481550b288c539326eeb
adolfo0620 <adolfo0620@gmail.com>
2015-01-31 21:53:51 -0500

    remove user and password from api and moved to a file called api_info.py and added to gitignore

--- bitrest/wrapperAPI.py
+++ bitrest/wrapperAPI.py
@@ -3,15 +3,14 @@ import json
 import time
 import base64
 from pprint import pprint as print
+from api_info import API_LOGIN
 
 
 class API():
-	def __init__( self):
-		user = 'bear'
-		password = 'bear'
+	def __init__(self):
 		url = '127.0.0.1'
 		port = 8442
-		url = "http://{}:{}@{}:{}/".format( user, password, url,port)
+		url = "http://{}:{}@{}:{}/".format( API_LOGIN["user"], API_LOGIN["password"], url,port)
 		self.api = client.ServerProxy( url )
 
 	def clean( self ):
@@ -57,4 +56,32 @@ class API():
 
 	def getAllMessagesIds(self):
 		ids = self.api.getAllInboxMessageIDs()
+<<<<<<< Updated upstream:bitrest/wrapperAPI.py
 		return(json.loads(ids)["inboxMessageIds"])
+=======
+		return(json.loads(ids)["inboxMessageIds"])
+
+
+if __name__ == "__main__":
+	a = API()
+	print(a.getAllMessages())
+
+'''
+[{'encodingType': 2,
+  'fromAddress': 'BM-2cUsUouTXohE15YA9sJzhghLCndnnxz2Ad',
+  'message': 'asdasdad',
+  'msgid': '2ea1235750e89a38d7751010fce7c62f0a46c292950db2abdf09612ee4bdcf66',
+  'read': 1,
+  'receivedTime': '1422460069',
+  'subject': 'asdasd',
+  'toAddress': 'BM-2cUTMYmgrpXmhieJLad7s5tLPnBQKaTyqb'},
+ {'encodingType': 2,
+  'fromAddress': 'BM-2cVr4jKDZNvBu4GeJvLqybgaaMCLqnv9Rr',
+  'message': 'fish fish fish fish fish fish fish',
+  'msgid': '30d23bc4174439738734db2e4bd9835308098fef2f31a049aca49840ddc90d90',
+  'read': 1,
+  'receivedTime': '1422461066',
+  'subject': 'fish',
+  'toAddress': 'BM-2cUTMYmgrpXmhieJLad7s5tLPnBQKaTyqb'}]
+'''
+>>>>>>> Stashed changes:bmapi/wrapperAPI.py
