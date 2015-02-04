7a7e26ca35474988e340f2211d10944d435c1c25
william <wmantly@gmail.com>
2015-02-04 16:44:14 -0500

    added middle ware token check

---
+++ bitR/middleware.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+from django.conf import settings
+from bitweb.models import User
+from bmapi.models import Token, BitKey
+from datetime import datetime, timedelta
+import json
+import uuid
+
+class tokenAuth( object ):
+    def process_request( self, request ):
+        # from pprint import pprint as p
+        # p( request )
+        path = request.path.split('/')
+        if path[1] != 'bmapi':
+            return None
+
+        print( path[2], settings.ALLOW_ROUTE)
+        if path[2] not in settings.ALLOW_ROUTE:
+            request.json = json.loads( request.body.decode('utf-8') )
+            token = uuid.UUID( request.json['token'] )
+
+            if Token.objects.filter(token=token).exists():
+                token = Token.objects.get( token=token )
+                if token.created_at.toordinal() > ( datetime.now() - timedelta(days=1) ).toordinal():
+                    request.json['_user'] = token.user
+                else:
+                    return JsonResponse( {}, status=401, content_type='application/json' )
