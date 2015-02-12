d8fda3d7d0397367f5b5096516319c0b5a2a4c04
Benjamin Himley <benjaminhimley85@gmail.com>
2015-02-12 12:33:11 -0500

    initializing files

---
+++ bitweb/static/js/model.js
@@ -0,0 +1,7 @@
+// when a user signs up, a new database should be initialized with the username, 
+// since that is a unique field name.  Orm if a user logs in and there is no 
+// database with their username created
+
+//  database should be locked unless the user the database is assigned to is 
+//  signed in
+
