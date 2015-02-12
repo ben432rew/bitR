42d7be5881c9806a312d9826f0f7a3da05967068
Benjamin Himley <benjaminhimley85@gmail.com>
2015-02-12 14:11:42 -0500

    intializing db schema, untested

--- bitweb/static/js/model.js
+++ bitweb/static/js/model.js
@@ -5,3 +5,102 @@
 //  database should be locked unless the user the database is assigned to is 
 //  signed in
 
+var schema = {
+  stores: [{
+    name: 'addressbook',
+    indexes: [{
+            name: 'address'
+        }, {
+            name: 'alias'
+    }]}, {
+    name: 'chan_subscriptions',
+    indexes: [{
+            name: 'label'
+        }, {
+            name: 'address'
+        }]
+    }, {
+    name: 'inbox_messages',
+    indexes: [{
+            name: 'message'
+        }, {
+            name: 'read'
+        }, {
+            name: 'toAddress'
+        }, {
+            name: 'receivedTime'
+        }, {
+            name: 'fromAddress'
+        }, {
+            name: 'subject'
+        }, {
+            name: 'msgid'
+        }, {
+            name: 'color'
+        }, {
+            name: 'inboxToAddress'
+        }, {
+            name: 'inboxMessage'
+        }, {
+            name: 'inboxSubject'
+        }, {
+            name: 'inboxFromAddress'
+        }]
+    }, {
+    name: 'sent_messages',
+    indexes: [{
+            name: 'message'
+        }, {
+            name: 'status'
+        }, {
+            name: 'toAddress'
+        }, {
+            name: 'receivedTime'
+        }, {
+            name: 'fromAddress'
+        }, {
+            name: 'subject'
+        }, {
+            name: 'msgid'
+        }, {
+            name: 'inboxMessage'
+        }, {
+            name: 'inboxSubject'
+        }, {
+            name: 'inboxFromAddress'
+        }, {
+            name: 'inboxToAddress'
+        }]
+    }, {
+    name: 'chanMessages',
+    indexes: [{
+            name: 'message'
+        }, {
+            name: 'read'
+        }, {
+            name: 'toAddress'
+        }, {
+            name: 'receivedTime'
+        }, {
+            name: 'fromAddress'
+        }, {
+            name: 'subject'
+        }, {
+            name: 'msgid'
+        }, {
+            name: 'chan'
+        }, {
+            name: 'color'
+        }, {
+            name: 'inboxMessage'
+        }, {
+            name: 'inboxSubject'
+        }, {
+            name: 'inboxFromAddress'
+        }, {
+            name: 'inboxToAddress'
+        }]
+    }
+  ]
+
+db = new ydn.db.Storage('practicing', schema)
