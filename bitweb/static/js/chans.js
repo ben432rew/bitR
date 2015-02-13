b9e2fe100162dd4da94cae7a59f9695e13c67ae7
Benjamin Himley <benjaminhimley85@gmail.com>
2015-02-13 13:06:11 -0500

    adding chans file

---
+++ bitweb/static/js/chans.js
@@ -0,0 +1,87 @@
+// populate chans list
+util.apiCall({
+    url: 'allchans',
+    callBack: function (data){
+        if (typeof data.chans == "string") {
+            window.location.replace('bmapi/logout');
+        } else {
+            data.chans.forEach(function(value) {
+                $.scope.chans.push(value);
+                $.scope.post_chan_list.push(value);
+                addressLookup.push({
+                    name: value.chan_label,
+                    address: value.chan_address
+                });
+            });
+            sessionStorage.setItem('chanNameList', JSON.stringify($.scope.chans));
+        }
+    }
+});
+
+// creates a chan
+$("#chan-form").submit(function(e){
+    e.preventDefault();
+    var info = {};
+    info.form = $("#chan_name").val();
+    $( '#chan-form' ).trigger('reset');
+    util.apiCall({
+        url: 'create_chan',
+        data: info,
+        callBack: function(data){
+            $.scope.chans.push(data);
+            $.scope.post_chan_list.push(data);
+            $('#create_chan').modal('toggle');
+
+        }
+    });            
+});
+
+// post message to chan, DRY out with above
+$( '#post_chan_form' ).on("submit", function(event) {
+    event.preventDefault();
+    var form_data = $(this).serializeObject();
+    form_data.send_addy = 'chan_post';
+    $( '#post_chan_form' ).trigger('reset');
+    util.apiCall({
+        url: 'send',
+        data: form_data
+    });
+});
+
+// join chan
+$( '#sub_chan_btn' ).click(function() { // why not submit
+    var info = {} ;
+    info.label = $( '#chan_label' ).val();
+    info.address = $( '#chan_addy' ).val();
+    util.apiCall({
+        url: 'joinchan',
+        data: info,
+        callBack: function (data){
+            if ( 'error' in data ){
+            } else {
+                $.scope.chans.push(data);
+                $.scope.post_chan_list.push(data);
+            }
+        }
+    });
+});
+
+// select chans to display 
+$('#chan_ul').on( 'click', 'li.jq-repeat-chans > label', function(){
+    var selected_chans = $('#chan_tab').text();
+    var val = '.' + $( this ).parent().attr('data-chan-add');
+    $( val ).toggle();
+});
+
+
+// show the chan tab
+$('#secondary').on( 'click', function(){
+    $('#identityDrop').hide()
+    $('.inbox-bucket').children().hide();
+    $( '#primary' ).removeClass( 'btn-material-blue-grey-100' );
+    $( this ).addClass( 'btn-material-blue-grey-100' );
+    $('#chan_tab').show()
+    $('#chan_mess').show();
+    // this next line is not working.  whyyyyyyyyyy
+    $( '.id_checks' ).prop('checked', false)
+});
--- bitweb/static/js/inbox.js
+++ bitweb/static/js/inbox.js
@@ -177,44 +177,6 @@
             });
         });
 
-        // creates a chan
-        $("#chan-form").submit(function(e){
-            e.preventDefault();
-            var info = {};
-            info.form = $("#chan_name").val();
-            $( '#chan-form' ).trigger('reset');
-            util.apiCall({
-                url: 'create_chan',
-                data: info,
-                callBack: function(data){
-                    $.scope.chans.push(data);
-                    $.scope.post_chan_list.push(data);
-                    $('#create_chan').modal('toggle');
-
-                }
-            });            
-        });
-
-        // populate chans list
-        util.apiCall({
-            url: 'allchans',
-            callBack: function (data){
-                if (typeof data.chans == "string") {
-                    window.location.replace('bmapi/logout');
-                } else {
-                    data.chans.forEach(function(value) {
-                        $.scope.chans.push(value);
-                        $.scope.post_chan_list.push(value);
-                        addressLookup.push({
-                            name: value.chan_label,
-                            address: value.chan_address
-                        });
-                    });
-                    sessionStorage.setItem('chanNameList', JSON.stringify($.scope.chans));
-                }
-            }
-        });
-
         // add new identity to list, select it
         $( '#create_id_button' ).click(function() {
             util.apiCall({
@@ -276,17 +238,6 @@
             });
         });
 
-        // post message to chan, DRY out with above
-        $( '#post_chan_form' ).on("submit", function(event) {
-            event.preventDefault();
-            var form_data = $(this).serializeObject();
-            form_data.send_addy = 'chan_post';
-            $( '#post_chan_form' ).trigger('reset');
-            util.apiCall({
-                url: 'send',
-                data: form_data
-            });
-        });
 
         // delete inbox message
         $( '#delete_msg' ).click(function() {
@@ -302,23 +253,6 @@
                 });
         });
 
-        // join chan
-        $( '#sub_chan_btn' ).click(function() { // why not submit
-            var info = {} ;
-            info.label = $( '#chan_label' ).val();
-            info.address = $( '#chan_addy' ).val();
-            util.apiCall({
-                url: 'joinchan',
-                data: info,
-                callBack: function (data){
-                    if ( 'error' in data ){
-                    } else {
-                        $.scope.chans.push(data);
-                        $.scope.post_chan_list.push(data);
-                    }
-                }
-            });
-        });
 
         // show sent messages
         $('.inbox-nav').on( 'click', 'button#sent', function(){
@@ -347,25 +281,6 @@
                 $('#sent-mess').show();
                 $( '#table-head' ).show();
             });
-        });
-
-        // show the chan tab
-        $('#secondary').on( 'click', function(){
-            $('#identityDrop').hide()
-            $('.inbox-bucket').children().hide();
-            $( '#primary' ).removeClass( 'btn-material-blue-grey-100' );
-            $( this ).addClass( 'btn-material-blue-grey-100' );
-            $('#chan_tab').show()
-            $('#chan_mess').show();
-            // this next line is not working.  whyyyyyyyyyy
-            $( '.id_checks' ).prop('checked', false)
-        });
-
-        // select chans to display 
-        $('#chan_ul').on( 'click', 'li.jq-repeat-chans > label', function(){
-            var selected_chans = $('#chan_tab').text();
-            var val = '.' + $( this ).parent().attr('data-chan-add');
-            $( val ).toggle();
         });
 
         // select identities to show 
