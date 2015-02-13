fdab39f1ce7278aa7d90ad7a7810d32144e74358
Benjamin Himley <benjaminhimley85@gmail.com>
2015-02-13 11:45:39 -0500

    users get their own db now

--- bitweb/static/js/model.js
+++ bitweb/static/js/model.js
@@ -1,11 +1,9 @@
-// when a user signs up, a new database should be initialized with the username, 
-// since that is a unique field name.  Orm if a user logs in and there is no 
-// database with their username created
-
 //  database should be locked and encrypted unless the user the database is 
 // assigned to is signed in
 
-var db = new ydn.db.Storage('practicing', bitrSchema)
+var username = $('#username').attr('data-username')
+
+var db = new ydn.db.Storage(username, bitrSchema)
 
 var localDB = {}
 
